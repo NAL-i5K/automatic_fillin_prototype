7ca2915da484e4b08ba10a0b8bd98a86f8820ba2
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-08-01 17:25:47 -0400

    finish request a new projcet page

--- index.js
+++ index.js
@@ -19,86 +19,56 @@ function clear_by_ids(ids) {
 $(function(){
   $('#organism_name').on('change paste keyup', function() {
     if ($('#organism_name').val() === '') {
+      clear_by_ids(['#tax_id', '#common_name', '#genus', '#species']);
       enable_by_ids(['#tax_id']);
-      disable_by_ids(['#organism-next']);
+      disable_by_ids(['#organism-search']);
     } else {
       disable_by_ids(['#tax_id']);
-      enable_by_ids(['#organism-next']);
+      enable_by_ids(['#organism-search']);
     }
   });
 
   $('#tax_id').on('change paste keyup', function() {
     if ($('#tax_id').val() === '') {
+      clear_by_ids(['#organism_name', '#common_name', '#genus', '#species']);
       enable_by_ids(['#organism_name']);
-      disable_by_ids(['#organism-next']);
+      disable_by_ids(['#organism-search']);
     } else {
       disable_by_ids(['#organism_name']);
-      enable_by_ids(['#organism-next']);
+      enable_by_ids(['#organism-search']);
     }
   });
 
-  $('#organism-next').click(function() {
+  $('#organism-search').click(function() {
     var tax_id = $('#tax_id').val();
     var orgn_name = $('#organism_name').val();
     if (tax_id !== '') {
-      $.getJSON('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=assembly&term=' + tax_id + '[TXID]' + '&retmode=json&usehistory=y',  function (data_esearch) {
-        var webenv = data_esearch['esearchresult']['webenv'];
-        var query_key = data_esearch['esearchresult']['querykey'];
-        $.getJSON('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=assembly&query_key=' + query_key + '&WebEnv=' + webenv + '&retmode=json', afterGetEsummaryData);
+      $.getJSON('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=taxonomy&id=' + tax_id + '&retmode=json',  function (data_esummary) {
+        if (data_esummary['result']['uids'].length === 0) {
+          $('#organism_name').val('No organism found, please check and search again.');
+        } else {
+          $('#organism_name').val(data_esummary['result'][tax_id]['scientificname']);
+          fill_fields(data_esummary);
+        }
       });
     } else {
-      $.getJSON('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=assembly&term=' + orgn_name + '[ORGN]' + '&retmode=json&usehistory=y',  function (data_esearch) {
-        var webenv = data_esearch['esearchresult']['webenv'];
-        var query_key = data_esearch['esearchresult']['querykey'];
-        $.getJSON('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=assembly&query_key=' + query_key + '&WebEnv=' + webenv + '&retmode=json', afterGetEsummaryData);
+      $.getJSON('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=taxonomy&retmode=json&term=' + orgn_name + '&retmode=json',  function (data_esearch) {
+        if (data_esearch['esearchresult']['count'] == 0) {
+          $('#tax_id').val('No organism found, please check and search again.');
+        } else {
+          var search_tax_id = data_esearch['esearchresult']['idlist'][0];
+          $('#tax_id').val(search_tax_id);
+          $.getJSON('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=taxonomy&id=' + search_tax_id + '&retmode=json', fill_fields);
+        }
       });
     }
   });
 });
 
-function afterGetEsummaryData(data_esummary) {
-  console.log(data_esummary);
-  var uids = data_esummary['result']['uids'];
-  var data = [];
-  for (var i = 0; i < uids.length; i++ ) {
-    data.push([
-      uids[i],
-      data_esummary['result'][uids[i]]['assemblyaccession'],
-      data_esummary['result'][uids[i]]['assemblyname'],
-      data_esummary['result'][uids[i]]['synonym']['genbank'],
-      data_esummary['result'][uids[i]]['synonym']['refseq'],
-    ]);
-  }
-  var table = $('#id-table').DataTable({
-    data: data,
-    columns: [
-        { title: "uid" },
-        { title: "assembly accession" },
-        { title: "assembly name"},
-        { title: "genbank" },
-        { title: "refseq" },
-    ],
-    searching: false,
-    info: false,
-    select: true,
-    autoWidth: false
-  });
 
-  $('#example tbody').on( 'click', 'tr', function () {
-    if ( $(this).hasClass('selected') ) {
-        $(this).removeClass('selected');
-    }
-    else {
-        table.$('tr.selected').removeClass('selected');
-        $(this).addClass('selected');
-    }
-  });
-
-  $($.fn.dataTable.tables(true)).css('width', '100%');
-  $('#id-table').css('visibility', 'visible');
-  $('#select-assembly').css('visibility', 'visible');
-  $('#select-assembly').click(function() {
-    console.log('selected item with assembly accession:' + table.rows({ selected: true }).data()[0][1]);
-  });
+function fill_fields(data_esummary) {
+  var uid = data_esummary['result']['uids'][0];
+  $('#common_name').val(data_esummary['result'][uid]['commonname']);
+  $('#genus').val(data_esummary['result'][uid]['genus']);
+  $('#species').val(data_esummary['result'][uid]['species']);
 }
-
